3da0ac9d737d1173ec4799546ff48ddbcc8c04f1
laurybenoit <lauryalexandre@me.com>
2022-01-30 17:18:27 -0500

    create all components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PropertiesComponent } from './properties/properties.component';
+import { PropertyListComponent } from './properties/property-list/property-list.component';
+import { PropertyDetailsComponent } from './properties/property-details/property-details.component';
+import { PropertyItemComponent } from './properties/property-list/property-item/property-item.component';
+import { LoginComponent } from './login/login.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProfileComponent } from './profile/profile.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PropertiesComponent,
+    PropertyListComponent,
+    PropertyDetailsComponent,
+    PropertyItemComponent,
+    LoginComponent,
+    FooterComponent,
+    ProfileComponent,
+   
   ],
   imports: [
     BrowserModule
